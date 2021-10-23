8b29d86cf9a1516e736606f46473f9570a1de972
iuiuiu-wayy <42693503+iuiuiu-wayy@users.noreply.github.com>
2021-10-23 17:51:59 +0700

    success run

--- portofolio/portofolio/portofolioapp/apps.py
+++ portofolio/portofolio/portofolioapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PortofolioappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portofolioapp'
+    name = 'portofolio.portofolioapp'
--- portofolio/portofolio/urls.py
+++ portofolio/portofolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portofolioapp.urls'))
+    path('', include('portofolio.portofolioapp.urls'))
 ]
